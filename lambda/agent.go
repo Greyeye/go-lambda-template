f18c227bb1e983c99492bba67988a184f988db24
Greyeye <james@makeabeeline.com>
2021-09-02 08:13:36 +1000

    add CORS header

--- lambda/agent.go
+++ lambda/agent.go
@@ -10,6 +10,12 @@ import (
 
 // getAgenthandler returns some data
 func (h *LambdaHandler) getAgenthandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token")
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 	requiredOptions := []string{"agentname"}
 	for _, v := range requiredOptions {
 		if _, ok := mux.Vars(r)[v]; !ok {
